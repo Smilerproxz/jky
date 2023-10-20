98427ad4822541d157e08dd501ddedde149e138c
Smilerproxz <131507750+Smilerproxz@users.noreply.github.com>
2023-10-20 18:10:20 -0400

    Add files via upload

---
+++ assets/0.2-adc6e0c7/js/helper.js
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404</title>
+	<style>
+		body {
+			background-color: #fff;
+			color: #00596d;
+			font-family: Arial, Helvetica, sans-serif;
+			margin: 0;
+			padding: 20%;
+		}
+		.error-page header {
+		}
+		.error-page header h1 small {
+			color: #bbb;
+			font-size: 70%;
+			font-weight: normal;
+		}
+		.error-page a {
+			color: inherit;
+			text-decoration: underline;
+		}
+
+		ul.games{
+			list-style: none;
+			padding: 0;
+			margin: 0 -10px;
+		}
+
+		ul.games li{
+			float: left;
+			box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			-webkit-box-sizing: border-box;
+			padding: 10px;
+			width: 25%;
+
+			text-align: center;
+
+		}
+
+		ul.games li a{
+			text-decoration: none;
+		}
+
+		ul.games li .btn-highlight{
+			display: none;
+		}
+
+		ul.games li .name{
+			font-size: 14px;
+			line-height: 16px;
+			height: 48px;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+
+
+		ul.games li img{
+			display: block;
+		}
+
+		ul.games li .icon{
+		border: 3px solid #FFF;
+			box-shadow: 0 0 3px rgba(0,0,0,.5);
+			margin-bottom: 5px;
+		}
+
+
+		img{
+			max-width: 100%;
+		}
+
+		@media  only screen and (max-width:700px){
+			body{
+				padding: 10%;
+			}
+		}
+
+		@media  only screen and (max-width:400px){
+			body{
+				padding: 20px;
+			}
+
+			ul.games li{
+				width: 50%;
+			}
+		}
+	</style>
+</head>
+<body class="error-page">
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PBT7JT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','famobi_dataLayer','GTM-PLWFGFS');</script><header>
+	<h1>404. <small>That's an error. Game not found. <a href="javascript:history.back();">Back</a></small></h1>
+</header>
+</body>
+</html>
